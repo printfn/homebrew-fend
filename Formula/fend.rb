1630e19135daa423da9e4175cd30566b01a5721e
printfn <printfn@users.noreply.github.com>
2022-03-12 13:42:17 +1300

    fend 0.1.29 -> 1.0.0

--- Formula/fend.rb
+++ Formula/fend.rb
@@ -1,8 +1,8 @@
 class Fend < Formula
   desc "Arbitrary-precision unit-aware calculator"
   homepage "https://printfn.github.io/fend-website"
-  url "https://github.com/printfn/fend/archive/refs/tags/v0.1.29.tar.gz"
-  sha256 "926e6d5c4c13573f2c93126ca9b6a82ad6cd270e9738ce07c5d9e246b4f84dbd"
+  url "https://github.com/printfn/fend/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "52a795d37bb9a5496f582cd05e42f5296f1e6c888f53ac603611d63df7ab865b"
   license "MIT"
   head "https://github.com/printfn/fend.git", branch: "main"
 
@@ -12,9 +12,7 @@ class Fend < Formula
   end
 
   bottle do
-    rebuild 1
-    root_url "https://github.com/printfn/homebrew-fend/releases/download/v0.1.29"
-    sha256 cellar: :any_skip_relocation, monterey: "9c30d031d1d3b037fec251f23f4b767fdabdfeabe11732909255231ecce69fed"
+    root_url "https://github.com/printfn/homebrew-fend/releases/download/v1.0.0"
   end
 
   depends_on "rust" => :build
