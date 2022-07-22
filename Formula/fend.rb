52fe27edaf7beafa9767e908214660c6c3217739
printfn <printfn@users.noreply.github.com>
2022-07-22 22:08:58 +1200

    fend 1.0.3 -> 1.0.4

--- Formula/fend.rb
+++ Formula/fend.rb
@@ -2,7 +2,7 @@ class Fend < Formula
   desc "Arbitrary-precision unit-aware calculator"
   homepage "https://printfn.github.io/fend-website"
   url "https://github.com/printfn/fend/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "167a42f734c546d97d44f52a29eb6e8422b7b683eb3a78546f290e7d2c6f31a129fbf096d0277523070576dcce1d5d762d1368ef0e6d20226f2f4cde5749b61d"
+  sha256 "b136937e5251160c17f46cb1b4ba80482e321bf70b82527856504c875ff0376f"
   license "MIT"
   head "https://github.com/printfn/fend.git", branch: "main"
 
